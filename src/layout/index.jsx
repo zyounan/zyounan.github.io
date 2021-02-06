7ede95699e72c34e31d4ddc773d1f3b65505d6e9
zyounan <923601132@qq.com>
2021-02-06 17:11:48 +0800

    Add: modify the template

---
+++ src/layout/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Header } from '../components/header'
+import { ThemeSwitch } from '../components/theme-switch'
+import { Footer } from '../components/footer'
+import { rhythm } from '../utils/typography'
+
+import './index.scss'
+
+export const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(30),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <ThemeSwitch />
+        {/* <Header title={title} location={location} rootPath={rootPath} /> */}
+        {children}
+        <Footer />
+      </div>
+    </React.Fragment>
+  )
+}
