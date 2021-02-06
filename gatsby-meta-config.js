2dd5607c6b222f44f464b6f97187ebc2b25a7b7c
zyounan <923601132@qq.com>
2021-02-06 17:13:11 +0800

    Add: config data

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `zyounan's blog`,
+  description: `Blog posted about ...`,
+  author: `zyounan`,
+  introduction: `这个人很懒，他什么都没有写`,
+  siteUrl: `https://zyounan.github.io`, // Your blog site url
+  social: {
+    twitter: ``,
+    github: ``,
+    medium: ``,
+    facebook: ``,
+    linkedin: ``,
+  },
+  icon: `static/favicon.jpg`,
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '',
+    utterances: '',
+  },
+  configs: {
+    countOfInitialPost: 10,
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '',
+  },
+  ga: '0',
+}
