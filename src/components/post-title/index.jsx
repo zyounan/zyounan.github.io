9ccab430219ecb6a23cadf106b1adae1e9cb82b0
zyounan <923601132@qq.com>
2021-02-07 17:15:19 +0800

    Add: back button

--- src/components/post-title/index.jsx
+++ src/components/post-title/index.jsx
@@ -1,3 +1,35 @@
 import React from 'react'
+import IconButton from '@material-ui/core/IconButton'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { lightBlue, indigo } from '@material-ui/core/colors'
+import { Link } from 'gatsby'
 
-export const PostTitle = ({ title }) => <h1>{title}</h1>
+
+import './index.scss'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: lightBlue,
+        secondary: indigo,
+    },
+});
+
+export const PostTitle = ({ title }) => {
+    return (
+        <div class="title-with-back-button">
+            <Link to="/">
+                <MuiThemeProvider theme={theme}>
+                    <IconButton color="primary" aria-label="Back"
+                    // onClick={history.back()}
+                    >
+                        <ArrowBackIcon />
+                    </IconButton>
+                </MuiThemeProvider>
+            </Link>
+            <h1>
+                {title}
+            </h1>
+        </div>
+    )
+}
